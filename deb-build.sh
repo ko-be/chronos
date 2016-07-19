ee8b6f8545627fe7053f79fb5dfee7ede55d5c35
Tobias Cole <tobyc@yelp.com>
2016-07-19 09:17:08 -0700

    Tidying up build script

--- deb-build.sh
+++ deb-build.sh
@@ -3,6 +3,8 @@
 # This script is designed to be run within the
 # Dockerfile.deb-build docker image
 
+# build targets are passed in as first parameter
+# values for this can be found in github mesosphere/chronos-pkg/Makefile
 TARGETS=$1
 
 cd ~
@@ -11,11 +13,15 @@ cd chronos-pkg
 rm -rf chronos
 ln -s /work chronos
 cd chronos
-MVN_VER=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep '^[0-9].*' | tail -n 1|xargs echo -n`
+PROJECT_VER=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version\
+    |grep '^[0-9].*' | tail -n 1|xargs echo -n`
 PKG_REL="0.1.`date -u +'%Y%m%d%H%M%S'`"
 cd ..
-make PKG_VER="$MVN_VER" PKG_REL="$PKG_REL" $TARGETS
+make PKG_VER="$PROJECT_VER" PKG_REL="$PKG_REL" $TARGETS
 cp *.deb chronos/dist
 
 # Create bintray config for travis
-sed -e "s/\${project_version}/$MVN_VER/" -e "s/\${build_timestamp}/`date -u +'%Y-%m-%d'`/" chronos/src/deb/bintray.json > chronos/dist/bintray.json
+# replaces placeholders with our vars
+sed -e "s/\${project_version}/$MVN_VER/"\
+    -e "s/\${build_timestamp}/`date -u +'%Y-%m-%d'`/"\
+    chronos/src/deb/bintray.json > chronos/dist/bintray.json
