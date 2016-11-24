ee9cea563b79e0e7e544c094da5cf2c8099b9c5f
Rob Johnson <rob@rob-johnson.me>
2016-11-24 14:37:08 +0000

    make scheduleStream type param covariant

--- src/main/scala/org/apache/mesos/chronos/scheduler/jobs/ScheduleStream.scala
+++ src/main/scala/org/apache/mesos/chronos/scheduler/jobs/ScheduleStream.scala
@@ -1,6 +1,6 @@
 package org.apache.mesos.chronos.scheduler.jobs
 
-import org.apache.mesos.chronos.schedule.Scheduling.Nextable
+import org.apache.mesos.chronos.schedule.Nextable
 
 /**
  * A stream of schedules.
@@ -8,7 +8,7 @@ import org.apache.mesos.chronos.schedule.Scheduling.Nextable
  * Each unit in the stream consists of a Schedule, a job name, and a schedule Time Zone (TODO: remove schedule time zone)
  * @author Florian Leibert (flo@leibert.de)
  */
-class ScheduleStream[T](val schedule: T, val jobName: String, val scheduleTimeZone: String = "")(implicit nextable: Nextable[T]) {
+class ScheduleStream[+T](val schedule: T, val jobName: String, val scheduleTimeZone: String = "")(implicit nextable: Nextable[T]) {
 
   def head: (T, String, String) = (schedule, jobName, scheduleTimeZone)
 
